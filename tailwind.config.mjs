65205c235baa688552487d799f92426c007d2d10
tobibaier0311 <tobias.baiier@web.de>
2024-01-24 21:07:59 +0100

    config tailwind & prettier

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      white: "#FEFAE0",
+      earth: "#DDA15E",
+      tiger: "#BC6C25",
+      lightgreen: "#595f39",
+      darkgreen: "#283618",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
