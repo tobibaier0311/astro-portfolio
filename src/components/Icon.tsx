f220e1c1b8122610376b400efc649ab570cc8c0e
tobibaier0311 <tobias.baiier@web.de>
2024-01-24 23:33:23 +0100

    add icon lib

---
+++ src/components/Icon.tsx
@@ -0,0 +1,13 @@
+import { Horse, Heart, Cube } from "@phosphor-icons/react";
+
+const Icon = () => {
+  return (
+    <main>
+      <Horse />
+      <Heart color="#AE2983" weight="fill" size={32} />
+      <Cube color="teal" weight="duotone" />
+    </main>
+  );
+};
+
+export default Icon;
