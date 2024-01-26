7305d47616056765bce942b9b79292d4af1dc6e0
tobibaier0311 <tobias.baiier@web.de>
2024-01-26 23:58:49 +0100

    Hero & About sections

---
+++ src/components/Icons.tsx
@@ -0,0 +1,61 @@
+import {
+  SiAstro,
+  SiFramer,
+  SiJavascript,
+  SiMui,
+  SiNextdotjs,
+  SiReact,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
+import Container from "./Container";
+
+const technologies = [
+  {
+    name: "JavaScript",
+    icon: <SiJavascript />,
+  },
+  {
+    name: "TypeScript",
+    icon: <SiTypescript />,
+  },
+  {
+    name: "React",
+    icon: <SiReact />,
+  },
+  {
+    name: "Next.js",
+    icon: <SiNextdotjs />,
+  },
+  {
+    name: "Tailwind",
+    icon: <SiTailwindcss />,
+  },
+  {
+    name: "Material UI",
+    icon: <SiMui />,
+  },
+  {
+    name: "Astro",
+    icon: <SiAstro />,
+  },
+  {
+    name: "Framer Motion",
+    icon: <SiFramer />,
+  },
+];
+
+export default function Icons() {
+  return (
+    <Container>
+      <div className="grid grid-cols-2 gap-2 mx-6 text-lg">
+        {technologies.map((item) => (
+          <div id={item.name} className="flex gap-3 items-center">
+            {item.icon}
+            {item.name}
+          </div>
+        ))}
+      </div>
+    </Container>
+  );
+}
--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -1,49 +1,10 @@
-import React from "react";
-import {
-  SiAstro,
-  SiFramer,
-  SiJavascript,
-  SiMui,
-  SiNextdotjs,
-  SiReact,
-  SiTailwindcss,
-  SiTypescript,
-} from "react-icons/si";
 import Container from "./Container";
+import Icons from "./Icons";
 
 export default function Technologies() {
   return (
     <Container>
-      <div className="text-white">
-        <div className="flex items-center gap-2">
-          <SiJavascript /> JavaScript
-        </div>
-        <div>
-          <SiTypescript /> TypeScript
-        </div>
-        <div>
-          {" "}
-          <SiReact /> React.js
-        </div>
-        <div>
-          {" "}
-          <SiNextdotjs /> Next.js
-        </div>
-        <div>
-          <SiAstro /> Astro
-        </div>
-        <div>
-          {" "}
-          <SiTailwindcss /> Tailwind
-        </div>
-        <div>
-          {" "}
-          <SiMui /> Material UI
-        </div>
-        <div>
-          <SiFramer /> Framer Motion
-        </div>
-      </div>
+      <Icons />
     </Container>
   );
 }
