3e94c69d43741e868d76fcaf55f61a069b7388c8
tobibaier0311 <tobias.baiier@web.de>
2025-04-07 20:54:38 +0200

    translations

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -13,11 +13,15 @@ export const ui = {
     "nav.about": "About",
     "hero.description":
       "aspiring frontend developer based in passau/regensburg, germany.",
+    "about.description":
+      "In my 3rd Semester of my Bachelor of Engineering Degree in Media Technology, main focus on Media Informatics, I discovered my passion for Frontend Web Development. I've been learning and improving ever since to be able to build and create beautiful user interfaces and experiences.",
   },
   de: {
     "nav.home": "Home",
     "nav.about": "Über mich",
     "hero.description":
       "aufstrebender Frontend-Entwickler mit Sitz in Passau/Regensburg, Deutschland.",
+    "about.description":
+      "Im dritten Semester meines Bachelor of Engineering in Medientechnik mit Schwerpunkt Medieninformatik entdeckte ich meine Leidenschaft für die Frontend-Webentwicklung. Seitdem lerne und verbessere ich mich kontinuierlich, um ansprechende Benutzeroberflächen und -erlebnisse zu entwickeln und zu gestalten.",
   },
 } as const;
