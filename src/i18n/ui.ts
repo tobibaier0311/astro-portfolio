4593203713c1680e105b2cb9b7be5bbce95dfdf6
tobibaier0311 <tobias.baiier@web.de>
2025-04-17 10:02:45 +0200

    LanguagePicker & logic refactor

---
+++ src/i18n/ui.ts
@@ -0,0 +1,28 @@
+export const languages = {
+  en: "English",
+  de: "Deutsch",
+};
+
+export const defaultLang = "en";
+
+export const showDefaultLang = false;
+
+export const routes = {
+  de: {
+    about: "about",
+    projects: "projects",
+  },
+};
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.projects": "Projects",
+  },
+  de: {
+    "nav.home": "Home",
+    "nav.about": "Über",
+    "nav.projects": "Projekte",
+  },
+} as const;
