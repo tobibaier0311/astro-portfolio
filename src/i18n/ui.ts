9a3fd29193b42a419ec1189fca2a2ed2c1f7aa11
tobibaier0311 <tobias.baiier@web.de>
2025-04-24 17:04:47 +0200

    add view transitions

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -19,10 +19,16 @@ export const ui = {
     "nav.home": "Home",
     "nav.about": "About",
     "nav.projects": "Projects",
+    "about.title": "about",
+    "about.description":
+      "My first professional work experience was from October 2022 - February 2023 as a Frontend Development Intern at a Data Science startup located in Salzburg, Austria.",
   },
   de: {
     "nav.home": "Home",
     "nav.about": "Über",
     "nav.projects": "Projekte",
+    "about.title": "about",
+    "about.description":
+      "Meine erste berufliche Erfahrung war von Oktober 2022 bis Februar 2023 als Frontend-Entwicklungspraktikant in einem Data-Science-Startup in Salzburg, Österreich.",
   },
 } as const;
