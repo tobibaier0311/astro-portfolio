773b692282df415b342ab0238d63acd4ad6b1d5b
tobibaier0311 <tobias.baiier@web.de>
2025-07-02 14:27:01 +0200

    fertig fast

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,33 @@
+import { CiMail } from "react-icons/ci";
+import type { ReactNode } from "react";
+import { LiaDownloadSolid } from "react-icons/lia";
+
+interface IconButtonProps {
+  icon?: ReactNode;
+  text?: string;
+  href?: string;
+  download?: boolean;
+  downloadIcon?: boolean;
+}
+
+const IconButton = ({
+  href,
+  download,
+  text,
+  downloadIcon,
+}: IconButtonProps) => {
+  return (
+    <a
+      className="flex justify-start items-center gap-1 font-extralight text-sm cursor-pointer hover:text-brand-4 transition-colors duration-500"
+      href={href}
+      download={download}
+    >
+      <div className="text-xs">
+        {downloadIcon ? <LiaDownloadSolid /> : <CiMail />}
+      </div>
+      {text}
+    </a>
+  );
+};
+
+export default IconButton;
--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -29,6 +29,15 @@ export const ui = {
     "about.description":
       "My first professional work experience was from October 2022 - February 2023 as a Frontend Development Intern at a Data Science startup located in Salzburg, Austria.",
     "about.technologies": "Here are technologies I mainly work with:",
+    "about.one":
+      "During my bachelor's degree, I discovered my passion for front-end development – ​​I've been fully involved ever since and am always curious to learn new things.",
+    "about.two":
+      "I like not only making things work, but also making them look good – whether through clean code or a coherent design.",
+    "about.three":
+      "I was able to gain my first practical experience during my internship in Salzburg, where I was able to work on exciting web projects.",
+    "about.four": "From the initial concept to the finished website.",
+    "about.five":
+      "I'm particularly proud of two websites I've created – most notably www.radio-webwelle.de, my bachelor's thesis. I developed the project entirely independently.",
     "projects.title": "projects",
     "projects.webwelle.title": "Radio Webwelle",
     "projects.webwelle.description":
@@ -39,7 +48,8 @@ export const ui = {
     "projects.bbaf.cta": "visit",
     "imprint.title": "Imprint",
     "privacy.title": "Privacy Policy",
-    "contact.me": "contact me",
+    "contact.me": "contact",
+    "download.cv": "cv",
   },
   de: {
     "hero.hi": "Hi,",
@@ -56,16 +66,27 @@ export const ui = {
       "Meine erste berufliche Erfahrung war von Oktober 2022 bis Februar 2023 als Frontend-Entwicklungspraktikant in einem Data-Science-Startup in Salzburg, Österreich.",
     "about.technologies":
       "Hier sind Technologien, mit denen ich hauptsächlich arbeite:",
+    "about.one":
+      "Im Laufe meines Bachelorstudiums habe ich meine Begeisterung für Frontend-Entwicklung entdeckt – seitdem bin ich voll dabei und immer neugierig, Neues dazuzulernen.",
+    "about.two":
+      "Ich mag es, Dinge nicht nur zum Laufen zu bringen, sondern sie auch gut aussehen zu lassen – ob durch sauberen Code oder durch ein stimmiges Design.",
+    "about.three":
+      "Erste praktische Erfahrungen konnte ich während meines Praxissemesters in Salzburg sammeln, wo ich an spannenden Webprojekten mitarbeiten durfte.",
+    "about.four": "Vom ersten Konzept bis zur fertigen Website",
+    "about.five":
+      "Besonders stolz bin ich auf zwei Websites, die ich umgesetzt habe – allen voran www.radio-webwelle.de, meine Bachelorarbeit. Das Projekt habe ich komplett eigenständig entwickelt.",
     "projects.title": "projekte",
     "projects.webwelle.title": "Radio Webwelle",
     "projects.webwelle.description":
       "Website für den Internetradiosender der Hochschule Deggendorf. Hier können Sie Hunderte von Radiosendungen, Podcasts usw. hören, die von Studenten gemacht und produziert wurden.",
+
     "projects.bbaf.title": "BaierBuam And Friends",
     "projects.bbaf.description":
       "Website für die BaierBuam und Freunde. Hier können Sie die neuesten Nachrichten, Veranstaltungen und vieles mehr finden.",
     "projects.bbaf.cta": "besuchen",
     "imprint.title": "Impressum",
     "privacy.title": "Datenschutzerklärung",
-    "contact.me": "Schreib mir",
+    "contact.me": "Kontakt",
+    "download.cv": "Lebenslauf",
   },
 } as const;
